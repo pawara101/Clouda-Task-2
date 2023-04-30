33467a36abc782a93db82b1d90b1189516fc4cca
pawara101 <pawara101dassanayake@gmail.com>
2023-04-30 17:55:09 +0530

    Clouda Task|2023-04-30 |v3

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ fetch(url)
         if(!response.ok){
             throw new Error(`Reques Failed ${response.status}`)
         }
-        console.log(response.status)
         return response.json()
     })
     .then(data =>{
