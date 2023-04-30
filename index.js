b4eb9dda40933b843a4cd7d29acc16c93369810d
pawara101 <pawara101dassanayake@gmail.com>
2023-04-30 10:14:48 +0530

    Clouda.io Task | 2023-04-30 | v2

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const fetch = require('node-fetch');
 const url = 'https://reqres.in/api/users';
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    .then(response => {
+        if(!response.ok){
+            throw new Error(`Reques Failed ${response.status}`)
+        }
+        console.log(response.status)
+        return response.json()
+    })
+    .then(data =>{
+        console.log(data)
+    })
+    .catch(error => console.log(error))
