fd4f1341127e5f5b87fdcbdad1b07f7bf6727135
pawara101 <pawara101dassanayake@gmail.com>
2023-06-21 01:35:57 +0530

    modified|2023-06-21

--- clouda_react_01/src/App.js
+++ clouda_react_01/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [user, setUser] = useState([]);
+
+  const fetchData = () => {
+    return fetch("https://reqres.in/api/users")
+          .then((response) => {
+            console.log(response.json())
+          })
+          .then((data) => setUser(data));
+
+  }
+  
+  useEffect(() => {
+    fetchData();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>User List</h1>
+    </main>
   );
 }
 
